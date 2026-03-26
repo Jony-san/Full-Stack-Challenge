c3ef2db8ea137cec4adc198e44cab2558b140544
Jony-san <jonathan.san.mejia@gmail.com>
2026-03-26 15:38:49 -0600

    Add: conexion con base de datos

---
+++ node-challenge/db/index.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+//Conexion con base de datos
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool);
