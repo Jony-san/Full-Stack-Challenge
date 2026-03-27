ab867043e67f02f59935ac324449bd638027942e
Jony-san <jonathan.san.mejia@gmail.com>
2026-03-26 20:11:32 -0600

    feat: implementacion de  RLS, ruta de consulta

--- node-challenge/app/api/[[...route]]/route.ts
+++ node-challenge/app/api/[[...route]]/route.ts
@@ -1,12 +1,62 @@
 import { Hono } from "hono";
+import { pool } from "@/db";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { contacts } from "@/db/schema";
+import type { PoolClient } from "pg";
 
 export const runtime = "nodejs";
 
-const app = new Hono().basePath("/api");
+type Variables = {
+  dbClient: PoolClient;
+};
+//Fijar base de rutas y tipear variales
+const app = new Hono<{ Variables: Variables }>().basePath("/api");
+
+//Para cada solicitud crear conexion y solicitar cabecera
+app.use("*", async (context, next) => {
+  const tenantId = context.req.header("x-tenant-id");
+
+  if (!tenantId) {
+    return context.json({ error: "Missing tenant" }, 400);
+  }
+
+  const client = await pool.connect();
+
+  try {
+    await client.query("BEGIN");
+
+    // ⚠️ IMPORTANTE: interpolación segura
+    const safeTenant = tenantId.replace(/'/g, "''");
+
+    await client.query(
+      `SET LOCAL app.current_tenant = '${safeTenant}'`
+    );
+
+    context.set("dbClient", client);
+
+    await next();
+
+    await client.query("COMMIT");
+  } catch (err) {
+    await client.query("ROLLBACK");
+    throw err;
+  } finally {
+    client.release();
+  }
+});
 
 app.get("/example", (context) => {
   console.log("Hono route hit");
   return context.json({ message: "API working" });
+});
+
+app.get("/contacts", async (context) => {
+  const client = context.get("dbClient");
+  const db = drizzle(client);
+
+  const result = await db.select().from(contacts);
+
+  return context.json(result);
 });
 
 app.all("*", (context) =>
